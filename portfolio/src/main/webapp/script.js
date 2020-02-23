9e9db4c7258885415adb0930bc08f968730e1f1c
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2020-02-22 21:03:14 -0800

    Revamp the website with resume info & stylize

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,3 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hi how are you?', 'Howdy', 'Hola!', 'How is your day going?'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
